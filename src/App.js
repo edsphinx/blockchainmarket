4ea6ac9d4db03a60b4688ac32c44ae90662c355b
Oscar Eduardo Fonseca Gomez <edsphinx@gmail.com>
2019-08-12 00:34:36 -0600

    fix netlify

--- src/App.js
+++ src/App.js
@@ -31,10 +31,10 @@ class App extends Component {
         //This to Deploy Ropsten Version And Comment Deploy Local Development Bellow
        //const contractAddress = "0x3620aC3b36A73D3AF16886C12D561132CED1F3Eb";
 
-        // const addressContract = "0x3620ac3b36a73d3af16886c12d561132ced1f3eb";
+        const addressContract = "0x3620ac3b36a73d3af16886c12d561132ced1f3eb";
         // const addressContract = "0x41874D4ae72E1Fc0F49F0401A5B04a1F3e26eb83";
-        // const contract = new web3.eth.Contract(BlockchainMarketContract.abi, addressContract);
-        const contract = new web3.eth.Contract(BlockchainMarketContract.abi, network && network.address);
+        const contract = new web3.eth.Contract(BlockchainMarketContract.abi, addressContract);
+        // const contract = new web3.eth.Contract(BlockchainMarketContract.abi, network && network.address);
 
         this.setState({
             web3,
